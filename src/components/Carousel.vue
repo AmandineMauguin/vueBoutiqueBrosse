120a62fa2c0cad798344013cb6b1af3c88f0431b
Amandine Mauguin <greenies@mbp-de-amandine-1.home>
2020-12-18 08:36:35 +0100

    8:36

---
+++ src/components/Carousel.vue
@@ -0,0 +1,37 @@
+<template>
+  
+  <button @click="addCount()">{{ count }}</button
+  ><transition name="fade"> <img :src="showImg()" /> </transition>`
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {};
+  },
+  methods: {
+    slideShow() {},
+    showImg() {
+      while (this.count < 0) {
+        this.count += this.liste.length;
+      }
+      while (this.count > this.liste.length - 1) {
+        this.count -= this.liste.length;
+      }
+      return this.liste[this.count];
+    },
+
+    addCount() {
+      this.count++;
+    },
+  },
+  mounted() {
+    setInterval(this.addCount, 5000);
+  },
+  computed: {},
+};
+</script> 
+
+<style scoped>
+</style>
---
+++ src/views/CartVue.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1></h1>
+  <Cart/>
+</template>
+
+<script>
+import Cart from '../components/Cart'
+export default {
+  name: "CartVue",
+  components: {Cart},
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+};
+</script> 
+
+<style scoped>
+</style>
